8d12e850bc31575c7981edc4ac11302f3ea834a7
lyaminartyom <lyaminartyom11@gmail.com>
2025-11-19 19:34:18 +0300

    scan + convert matheds signatures

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,8 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USD_TO_EUR = 0.8531
 	const USD_TO_RUB = 81.1276
 	const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
+}
+
+func convert(amount float64, scrCurrency string, dstCurrency string) {
+
+}
+
+func scan() (float64, string, string) {
+	var amount float64
+	var src, dst string
+
+	fmt.Scan(&amount, &src, &dst)
+
+	return amount, src, dst
 }
