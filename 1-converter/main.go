6268b9abaef606fc617ae2aabe72fdfce63dabe3
lyaminartyom <lyaminartyom11@gmail.com>
2025-11-23 18:22:47 +0300

    use referencea instead copy

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,37 +6,38 @@ const USD = "USD"
 const RUB = "RUB"
 const EUR = "EUR"
 
-var exchangeRates = map[string]float64{
-	USD: 1.0,
-	EUR: 0.8531,
-	RUB: 81.1276,
-}
-
 func main() {
-	curr := scanCurrency()
+
+	var exchangeRates = map[string]float64{
+		USD: 1.0,
+		EUR: 0.8531,
+		RUB: 81.1276,
+	}
+
+	curr := scanCurrency(&exchangeRates)
 	amount := scanAmount()
 	fmt.Println("Input currency to convert:")
-	dst := scanCurrency()
-	result := convert(amount, curr, dst)
+	dst := scanCurrency(&exchangeRates)
+	result := convert(&exchangeRates, amount, curr, dst)
 	fmt.Printf("%.2f %s = %.2f %s\n", amount, curr, result, dst)
 }
 
-func convert(amount float64, srcCurrency string, dstCurrency string) float64 {
-	amountInUSD := amount / exchangeRates[srcCurrency]
-	return amountInUSD * exchangeRates[dstCurrency]
+func convert(exchangeRates *map[string]float64, amount float64, srcCurrency string, dstCurrency string) float64 {
+	amountInUSD := amount / (*exchangeRates)[srcCurrency]
+	return amountInUSD * (*exchangeRates)[dstCurrency]
 }
 
-func scanCurrency() string {
+func scanCurrency(exchangeRates *map[string]float64) string {
 	curr := ""
-	for !isValidCurrency(curr) {
+	for !isValidCurrency(exchangeRates, curr) {
 		fmt.Println("Input currency: RUB, EUR, USD")
 		fmt.Scan(&curr)
 	}
 	return curr
 }
 
-func isValidCurrency(curr string) bool {
-	_, exists := exchangeRates[curr]
+func isValidCurrency(exchangeRates *map[string]float64, curr string) bool {
+	_, exists := (*exchangeRates)[curr]
 	return exists
 }
 
